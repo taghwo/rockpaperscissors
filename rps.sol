f729cb781525e13d50c2efe7662994e684a4b769
Taghwo Millionaire <40868373+taghwo@users.noreply.github.com>
2021-08-22 12:19:38 +0100

    Update rps.sol

--- rps.sol
+++ rps.sol
@@ -3,10 +3,12 @@
  
  contract RPS{
      address owner;
+     
      mapping(string => mapping(string => string)) winningCombination;
+     
      struct Game{
-         uint256 player1PlayedTime;
-         uint256 player2PlayedTime;
+         uint256 player1CurrentRound;
+         uint256 player2CurrentRound;
          uint256 player1Score;
          uint256 player2Score;
          uint256 totalPlays;
@@ -20,7 +22,6 @@
          string player2CurrentHand;
          bool earningCleared;
          bool isActive;
-        
      }
      
      mapping(string => Game) games;
@@ -28,23 +29,13 @@
      mapping(string => uint256) hands;
      
      mapping(address => uint256) totalEarning;
-     
-     modifier isGameOpen(string calldata name){
-         require(games[name].player2 == address(0), "Game already full");
+    
+     modifier UnauthorizedAccess(string calldata name){
+         require(games[name].player1 == msg.sender || games[name].player2 == msg.sender, "Not eligible to play this game");
          _;
      }
      
-     modifier playerInGame(string calldata name){
-         require(games[name].player1 != msg.sender || games[name].player2 != msg.sender, "You already joined the game");
-         _;
-     }
-     
-     modifier playerHasAccess(string calldata name){
-         require(games[name].player1 != msg.sender || games[name].player2 != msg.sender, "You cannot play this game because you were not registered to play");
-         _;
-     }
-     
-     modifier findExistingGame(string calldata name){
+     modifier ExistingActiveGame(string calldata name){
          require(keccak256(bytes(games[name].name)) == keccak256(bytes(name)) && games[name].isActive == false, 'Game does not exist or completed');
          _;
      }
@@ -55,9 +46,10 @@
      event Drawn(string indexed name, address indexed player1, address indexed player2);
      event Won(string indexed name, address indexed player, uint256 amountWon);
      event Paid(string indexed name, address indexed player, uint256 amountWon);
-     event Withdrawn(uint256 amount, address indexed player);
+     event Withdrew(uint256 amount, address indexed player);
      event Cancelled(string indexed name, address indexed player, uint256 cancelledAt);
      
+     ///Initiliaze attributes
      constructor(){
          owner = msg.sender;
          
@@ -73,78 +65,97 @@
          winningCombination['paper']['scissors'] = 'scissors';
      }
      
-     function newGame(string calldata name) external payable playerInGame(name) returns(bool){
-        require(games[name].player1 == address(0), "This game already exists");
+     /// @dev Create new game and set msg.sender as player1
+     /// @param name of Game
+     function newGame(string calldata name) external payable{
+        require(keccak256(bytes(games[name].name)) != keccak256(bytes(name)), "This game already exists");
         require(msg.value != 0, "Please add a wager value");
-        games[name] = Game({name:name, player1:msg.sender, player2:address(0), player1PlayedTime:0, player2PlayedTime:0, player1Score:0, player2Score:0, 
-                            player1CurrentHand:'', player2CurrentHand:'', winner:address(0), earningCleared:false, isActive:true, totalPlays:0, totalStake:msg.value,
-                            leastStakeAbleAmount:msg.value}) ;
- 
-        emit Created(name, msg.value, msg.sender);
         
-        return true;
+        games[name].name = name;  
+        games[name].player1 = msg.sender;               
+        games[name].player2 = address(0);
+        games[name].player1CurrentRound = 0;
+        games[name].player2CurrentRound = 0;
+        games[name].player1Score = 0;
+        games[name].player2Score = 0;
+        games[name].player1CurrentHand = '';
+        games[name].player2CurrentHand = '';
+        games[name].winner = address(0);
+        games[name].earningCleared = false;
+        games[name].isActive = true;
+        games[name].totalPlays = 0;
+        games[name].totalStake = msg.value;
+        games[name].leastStakeAbleAmount = msg.value;
+
+        emit Created(name, msg.value, msg.sender);
      }
      
-     function getGame(string calldata name) external view findExistingGame(name) playerInGame(name) returns(Game memory){
+     /// @dev handles cancelling and refunds
+     /// @param name of Game
+     function getGame(string calldata name) external view ExistingActiveGame(name) UnauthorizedAccess(name) returns(Game memory){
         return games[name];
      }
     
-     function joinGame(string calldata name) external payable findExistingGame(name) playerInGame(name) isGameOpen(name) returns(bool){
+     /// @dev Register new player to game
+     /// @param name of Game
+     function joinGame(string calldata name) external payable ExistingActiveGame(name){
+        require(games[name].player2 == address(0), "Game is full");
+        require(games[name].player1 != msg.sender || games[name].player2 != msg.sender, "You are in this game already");
         require(games[name].leastStakeAbleAmount == msg.value, 
         string(abi.encodePacked("The money you staked is below or above minimum stake-able amount which is"," ","games[name].leastStakeAbleAmount"))
         );
+        
         games[name].player2 = msg.sender;
         games[name].totalStake += msg.value;
+        
         emit Registered(name, msg.value, msg.sender);
-        return true;
      }
      
-     function play(string calldata name, string calldata handPlayed) external findExistingGame(name) playerHasAccess(name) returns(string memory){
+     /// @dev Play game. Game rounds starts with player 
+     /// @param name of Game
+     /// @param handPlayed based on hands
+     function play(string calldata name, string calldata handPlayed) external ExistingActiveGame(name) UnauthorizedAccess(name){
         Game storage game = games[name];
         
         require(game.player2 != address(0), "Waiting for Player 2 to join");
-        
         require(hands[handPlayed] != 0, "Invalid hand played. Options: rock, scissors and paper");
 
-        if(game.player1 == msg.sender && game.player1PlayedTime > game.player2PlayedTime){
-            return "Waiting for player 2 to play";
+        if(game.player1 == msg.sender && game.player1CurrentRound > game.player2CurrentRound){
+            revert("Waiting for player 2 to play");
         }
         
-        if(game.player2 == msg.sender && game.player2PlayedTime > game.player1PlayedTime){
-            return "Waiting for player 1 to play";
+        if(game.player2 == msg.sender && game.player2CurrentRound > game.player1CurrentRound){
+            revert("Waiting for player 1 to play");
         }
          
         if(game.player1 == msg.sender){
             game.player1CurrentHand = handPlayed;
-            game.player1PlayedTime = block.timestamp;
+            game.player1CurrentRound += 1 ;
         }
         
         if(game.player2 == msg.sender){
             game.player2CurrentHand = handPlayed;
-            game.player2PlayedTime = block.timestamp;
+            game.player2CurrentRound += 1 ;
         }
+        
+         game.totalPlays++;
          
-         if(game.player2 == msg.sender){
-             if(keccak256(bytes(game.player1CurrentHand)).length > 0){
-                computePlay(game);
+         if(game.player2 == msg.sender && keccak256(bytes(game.player1CurrentHand)).length > 0){
+                computeRound(game);
                 game.player1CurrentHand = '';
                 game.player2CurrentHand = '';
-                game.totalPlays++;
-             }
-             
-             if(game.totalPlays == 6){
-                 emit Played(name, handPlayed, msg.sender);
-                 updateEarningAndCloseGame(game);
-                 return "Game over, winner total earning updated";
-             }
          }
          
-        emit Played(name, handPlayed, msg.sender);
-        
-        return "You have played your hand";
+         if(game.totalPlays == 6){
+            updateEarningAndCloseGame(game);
+         }
+         
+         emit Played(name, handPlayed, msg.sender);
      }
-  
-     function computePlay(Game storage game) internal{
+     
+     /// @dev Compute score for round played
+     /// @param game instance of Game
+     function computeRound(Game storage game) internal{
         
          if(keccak256(bytes(game.player1CurrentHand)) == keccak256(bytes(game.player2CurrentHand))){
             game.player1Score += 1;
@@ -160,54 +171,68 @@
          }
      } 
      
+     /// @dev fetch score of player1Score and player2Score
+     /// @param name of Game
+     /// @return player1Score and player2Score 
      function result(string calldata name) external view returns(uint256 player1Score, uint256 player2Score){
           player1Score = games[name].player1Score;
           player2Score = games[name].player2Score;
      }
      
-     
+     /// @dev Update earnings and close game
+     /// @param game instance OF Game
      function updateEarningAndCloseGame(Game storage game) internal{
          if(game.player1Score == game.player2Score){
               totalEarning[game.player1] += game.leastStakeAbleAmount;
               totalEarning[game.player1] += game.leastStakeAbleAmount;
+              
               emit Drawn(game.name, game.player1, game.player2);
           }else if(game.player1Score > game.player2Score){
                game.winner = game.player1;
+               totalEarning[game.winner] += game.totalStake;
           }else{
                game.winner = game.player2;
+               totalEarning[game.winner] += game.totalStake;
           }
           
-          totalEarning[game.winner] += game.totalStake;
           game.earningCleared = true;
           game.isActive = false;
+          
           emit Won(game.name, game.winner, game.totalStake);
      }
      
+     /// @dev Fetch total earning for an address
+     /// @return uint256 totalEarnings
      function totalEarnings() external view returns(uint256){
           return totalEarning[msg.sender];
      }
      
-     function withdraw(uint256 amount) external payable returns(string memory){
+     /// @dev Initiliaze withdrawal from totalEarning based on address 
+     /// @param amount TO withdraw
+     function withdraw(uint256 amount) external payable{
           require(amount > 0, 'Minimum Withdrawal is 1');
-          require(totalEarning[msg.sender] >= amount, 'Sorry you cannot withdraw above your total earnings');
+          require(totalEarning[msg.sender] >= amount, 'Insufficient funds');
+         
           totalEarning[msg.sender] -= amount;
+          
           bool isSent = payable(msg.sender).send(amount);
           
           if(! isSent){
               revert('Withdrawal failed');
           }
           
-          emit Withdrawn(amount, msg.sender);
-          return "Withdrawal successful";
+          emit Withdrew(amount, msg.sender);
      }
      
-    function cancel(string calldata name, uint256 cancelledAt) external findExistingGame(name) playerHasAccess(name) returns(string memory){
+     /// @dev Cancel and refund handler
+     /// @param name of Game
+     /// @param cancelledAt time in seconds
+     function cancel(string calldata name, uint256 cancelledAt) external ExistingActiveGame(name) UnauthorizedAccess(name){
           Game storage game = games[name];
-          
           address cancelledBy = msg.sender;
           
           if(game.player2 != address(0) && keccak256(bytes(game.player1CurrentHand)) != keccak256(bytes(''))){
-              return "You cannot cancel this game, because player two already joined";
+              revert("You cannot cancel this game, because player two already joined");
           }
           
           if(game.player2 != address(0)){
@@ -218,8 +243,7 @@
           }
           
           game.isActive = false;
-          emit Cancelled(name, cancelledBy, cancelledAt);
           
-          return "Game was cancelled";
+          emit Cancelled(name, cancelledBy, cancelledAt);
      }
  }
